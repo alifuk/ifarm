1c54b4108588c8c16a508997d2be16a4089b7988
alifuk <amdalifuk@gmail.com>
2015-05-31 16:26:44 +0200

    hotový rozvržení - pousmaž loggedIn

--- cz/indexAlbi.php
+++ cz/indexAlbi.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 include './queries/setDefaultCategory.php';
-
 print_r($_SESSION['kategorie']);
 ?>
 
@@ -9,6 +8,7 @@ print_r($_SESSION['kategorie']);
 
 <html>
     <head>
+        <!--
         <meta charset="UTF-8">
         <title></title>     
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
@@ -16,45 +16,54 @@ print_r($_SESSION['kategorie']);
         <link rel="stylesheet" href="./bootstrap-3.1.1-dist/css/bootstrap.min.css">
         
         <link rel="stylesheet" href="./bootstrap-3.1.1-dist/css/bootstrap-theme.min.css">
-        <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-        <!-- Optional theme -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-
-        <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-
+        
         <script src="./bootstrap-3.1.1-dist/js/jquery-2.1.4.min.js"></script>  <script>
-
+        
             /*$(document).ready(function(){
              $('[data-toggle="tooltip"]').tooltip();   
              });*/
-        </script>
+        </script> -->
+
+        <?php include './head.php'; ?>
     </head>
+
+
+
     <body>
-<?php
-if (isset($_SESSION['user']) && $_SESSION['user'] != "") {
 
 
-    include './forms/navbar.php';
-
-    include './queries/mojeSablony.php';
-    include './queries/profileEditQ.php';
-
-    include './forms/naseptavac.php';
-    include './forms/kategorie.php';
-
-    include './forms/sablona.php';
 
 
-    include './forms/aresForm.php';
-} else {
-    include './forms/loginForm.php';
-    include './forms/registerForm.php';
-}
+        <?php
+        include './menu.php';
 
-// put your code here
-?>
-    </body>
-</html>
+        if (isset($_SESSION['user']) && $_SESSION['user'] != "") {
+
+            /*
+              include './forms/navbar.php';
+
+              include './queries/mojeSablony.php';
+              include './queries/profileEditQ.php';
+
+              include './forms/naseptavac.php';
+              include './forms/kategorie.php';
+
+              include './forms/sablona.php';
+
+
+              include './forms/aresForm.php';
+             * 
+             * 
+             * 
+             * 
+             */
+
+            include './indexlogin.php';
+        } else {
+            include './forms/loginForm.php';
+            include './forms/registerForm.php';
+        }
+        ?>
